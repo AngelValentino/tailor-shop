d8610bb499b5ea3935b18f96ab116ead09882403
Jack <angelvalentino977@gmail.com>
2025-10-12 15:13:30 +0200

    chore: improve html structure and add test data object for garment slider

---
+++ src/data/MannequinInfo.js
@@ -0,0 +1,188 @@
+export default MannequinInfo = {
+  jacket2: {
+    title: 'Built Biker Jacket',
+    description: 'This biker jacket was made in Japan from medium weight leather and features hand finished details for a vintage inspired look.',
+    images: [
+      {
+        large: [
+          {
+            url: '/images/jacket2/1.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/2.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/3.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/4.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/5.jpg',
+            alt: ''
+          },
+        ],
+        medium: [
+          {
+            url: '/images/jacket2/1-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/2-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/3-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/4-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/5-regular.jpg',
+            alt: ''
+          },
+        ],
+        small: [
+          {
+            url: '/images/jacket2/1-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/2-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/3-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/4-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/jacket2/5-small.jpg',
+            alt: ''
+          },
+        ]
+      }
+    ]
+  },
+  pleatedTrousers: {
+    title: 'High waisted pleated trousers',
+    description: 'Vintage modern looking denim inspired high waisted pleated trousers with side belts and 5 pockets,',
+    images: [
+      {
+        large: [
+          {
+            url: '/images/pleated-trousers/1.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/2.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/3.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/4.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/5.jpg',
+            alt: ''
+          },
+        ],
+        medium: [
+          {
+            url: '/images/pleated-trousers/1-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/2-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/3-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/4-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/5-regular.jpg',
+            alt: ''
+          },
+        ],
+        small: [
+          {
+            url: '/images/pleated-trousers/1-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/2-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/3-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/4-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/pleated-trousers/5-small.jpg',
+            alt: ''
+          },
+        ]
+      }
+    ]
+  },
+  bomberOutfit: {
+    title: 'Bomber and mid waisted trousers outfit',
+    description: 'Complet outfit bomber flight jacket casual formal style combined with a proper mid waisted trousers.',
+    images: [
+      {
+        large: [
+          {
+            url: '/images/bomber5-outfit/1.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/bomber5-outfit/2.jpg',
+            alt: ''
+          }
+        ],
+        medium: [
+          {
+            url: '/images/bomber5-outfit/1-regular.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/bomber5-outfit/2-regular.jpg',
+            alt: ''
+          }
+        ],
+        small: [
+          {
+            url: '/images/bomber5-outfit/1-small.jpg',
+            alt: ''
+          },
+          {
+            url: '/images/bomber5-outfit/2-small.jpg',
+            alt: ''
+          }
+        ]
+      }
+    ]
+  }
+};

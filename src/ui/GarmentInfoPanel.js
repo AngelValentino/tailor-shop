eb1147ba13206deae8c2858adc4d76a8e10ac4cb
Jack <angelvalentino977@gmail.com>
2025-10-14 18:21:17 +0200

    refactor: remove unnecessary methods from GarmentInfoPanel

--- src/ui/GarmentInfoPanel.js
+++ src/ui/GarmentInfoPanel.js
@@ -85,45 +85,4 @@ export default class GarmentInfoPanel {
       `
     );
   }
-
-
-
-
-
-  setTailorShopExperience(tailorShopExperience) {
-    this.tailorShopExperience = tailorShopExperience;
-  }
-
-  cloneMannequinHandler(activeMannequin) {
-    return () => {
-      this.tailorShopExperience.cloneActiveMannequin(activeMannequin);
-    };
-  }
-
-  toggleUiBtn(activeMannequin) {
-    this.btnContainerLm.classList.add('visible');
-
-    if (this.btnContainerLm.classList.contains('visible')) {
-      if (!this.eventHandler.clone) {
-        this.eventHandler.clone = this.cloneMannequinHandler(activeMannequin);
-        this.viewMoreBtn.addEventListener('click', this.eventHandler.clone);
-      }
-
-      if (!this.eventHandler.restore) {
-        this.eventHandler.restore = this.tailorShopExperience.restoreOppositeSide.bind(this.tailorShopExperience);
-        this.returnBtn.addEventListener('click', this.eventHandler.restore);
-      }
-    } 
-  }
-
-  hideRestoreBtn() {
-    this.returnBtn.removeEventListener('click', this.eventHandler.restore);
-    this.viewMoreBtn.removeEventListener('click', this.eventHandler.clone);
-
-    this.eventHandler.restore = null;
-    this.eventHandler.clone = null;
-
-    console.log('left UI and removed events');
-    this.btnContainerLm.classList.remove('visible');
-  }
 }
